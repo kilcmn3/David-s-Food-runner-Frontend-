4cb8c57b0d659a171cac70e18d7c766b9e87df0d
kilcmn <kilcmn3@hotmail.com>
2020-04-12 18:44:59 -0400

    fetching

--- src/containers/RestaurantsListContainer.js
+++ src/containers/RestaurantsListContainer.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 import RestaurantCard from '../components/RestaurantCard';
+import * as requests from './requests'
+
 export default class RestaurantsListContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      restaurants: []
+    }
+  }
+  componentDidMount() {
+    requests.fetchRestaurants().then(restaurants => console.log(restaurants))
+  }
   render() {
     return (
       <div>
---
+++ src/containers/requests.js
@@ -0,0 +1,17 @@
+// headers --> use these at your own discretion
+// const headers = {'Content-Type': 'application/json', 'Accepts': 'application/json'}
+// urls
+const restaurantsURL = 'http://localhost:4000/restaurants';
+// parse incoming data
+const parseData = (response) => response.json();
+// error handler
+const catchError = (error) => console.log(`%c${error}`, 'color: red;');
+
+//////////////////////////////////////////////////////
+
+// Fetches for kaijus, will return a promise
+// GET /kaijus
+export const fetchRestaurants = () =>
+    fetch(restaurantsURL).then(parseData).catch(catchError);
+
+// TODO: define a few more kaiju fetches
