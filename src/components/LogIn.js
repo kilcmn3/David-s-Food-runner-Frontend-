6b07425c61463f15011981bf952e2b23483d05b0
kilcmn3 <kilcmn3@hotmail.com>
2021-04-10 12:18:02 -0400

    login container headline align to center

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -48,46 +48,46 @@ const LogIn = (props) => {
           handleBlur,
           handleSubmit,
           isSubmitting }) => (
-            <MYFORM onSubmit={handleSubmit} className="mx-auto">
-              <Form.Group controlId="formEmail">
-                <Form.Label>User Name :</Form.Label>
-                <Form.Control
-                  name="email"
-                  type="text"
-                  placeholder="Enter your email"
-                  value={values.email}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  className={errors.email && touched.email && "error"}
-                />
-                {touched.email && errors.email ? (
-                  <div className="error-message">{errors.email}</div>
-                ) : null}
-              </Form.Group>
-              <Form.Group controlId="formPaSSWORD">
-                <Form.Label>Password :</Form.Label>
-                <Form.Control
-                  name="password"
-                  type="password"
-                  placeholder="Enter your password"
-                  value={values.password}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  className={errors.password && touched.password && "error"}
-                />
-                {touched.password && errors.password ? (
-                  <div className="error-message">{errors.password}</div>
-                ) : null}
-              </Form.Group>
-              {/*Submit button that is disabled after button is clicked/form is in the process of submitting*/}
-              <BUTTON variant="primary" type="submit" disabled={isSubmitting}>
-                Sign In
+          <MYFORM onSubmit={handleSubmit} className="mx-auto">
+            <Form.Group controlId="formEmail">
+              <Form.Label>User Name :</Form.Label>
+              <Form.Control
+                name="email"
+                type="text"
+                placeholder="Enter your email"
+                value={values.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className={errors.email && touched.email && "error"}
+              />
+              {touched.email && errors.email ? (
+                <div className="error-message">{errors.email}</div>
+              ) : null}
+            </Form.Group>
+            <Form.Group controlId="formPaSSWORD">
+              <Form.Label>Password :</Form.Label>
+              <Form.Control
+                name="password"
+                type="password"
+                placeholder="Enter your password"
+                value={values.password}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className={errors.password && touched.password && "error"}
+              />
+              {touched.password && errors.password ? (
+                <div className="error-message">{errors.password}</div>
+              ) : null}
+            </Form.Group>
+            {/*Submit button that is disabled after button is clicked/form is in the process of submitting*/}
+            <BUTTON variant="primary" type="submit" disabled={isSubmitting}>
+              Sign In
           </BUTTON>
-              <Link to='/signup'><BUTTON variant="primary" type="submit">
-                Register
+            <Link to='/signup'><BUTTON variant="primary" type="submit">
+              Register
           </BUTTON></Link>
-            </MYFORM>
-          )}
+          </MYFORM>
+        )}
       </Formik>
     </CONTAINER>
   );
