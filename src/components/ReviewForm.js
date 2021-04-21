912b04e271e2b8ba9d0921fafb159f5a30fe88f9
kilcmn3 <kilcmn3@hotmail.com>
2021-04-21 10:51:25 -0400

    before install prettier and eslint confi0g

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -3,19 +3,16 @@ import { Form, Button, Container, Row, Col } from 'react-bootstrap';
 
 const ReviewForm = (props) => {
   return (
-    <Container>
-      <Row className="justify-content-md-center">
-        <Col></Col>
-        <Col>
-          <Form inline onSubmit={props.handleSubmit}>
-            <Form.Group controlId="formInlineName">
-              <Form.Control value={props.comment} type="text" onChange={props.handleChange} placeholder="write your commnets here.." />
-            </Form.Group>
-            <Button type="submit">Submit</Button>
-          </Form>
-        </Col>
-      </Row>
-    </Container >
+
+
+    <Form inline onSubmit={props.handleSubmit}>
+      <Form.Group controlId="formInlineName">
+        <Form.Control value={props.comment} type="text" onChange={props.handleChange} placeholder="write your commnets here.." />
+      </Form.Group>
+      <Button type="submit">Submit</Button>
+    </Form>
+
+
   )
 }
 
