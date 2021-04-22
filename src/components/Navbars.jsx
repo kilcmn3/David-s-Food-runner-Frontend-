47367f156ea876a1afc3b37c9b7d9176687324b4
kilcmn3 <kilcmn3@hotmail.com>
2021-04-22 18:57:53 -0400

    need to clear Nav Link correctly

--- src/components/Navbars.jsx
+++ src/components/Navbars.jsx
@@ -10,11 +10,11 @@ import { SearchBar } from '../exportComponents';
 const Navbars = (props) => {
   return (
     <Navbar bg='light' expand='lg'>
-      <Navbar.Brand href='Home'>Home</Navbar.Brand>
+      <Navbar.Brand href='/home'>Home</Navbar.Brand>
       <Navbar.Toggle aria-controls='basic-navbar-nav' />
       <Navbar.Collapse id='basic-navbar-nav'>
         <Nav className='mr-auto'>
-          <Nav.Link href='#home'>Home</Nav.Link>
+          <Nav.Link href='/home'>Home</Nav.Link>
           <Nav.Link href='#link'>Link</Nav.Link>
         </Nav>
         <Form inline>
