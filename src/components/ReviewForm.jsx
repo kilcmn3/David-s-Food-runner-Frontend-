d60fed22cb046daf62a7f9b523dcee8da9ad5e4e
kilcmn3 <kilcmn3@hotmail.com>
2021-04-23 15:16:34 -0400

    changed form type;deleted form.inline attribute

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -3,7 +3,7 @@ import { Form, Button } from 'react-bootstrap';
 
 const ReviewForm = (props) => {
   return (
-    <Form inline onSubmit={props.handleSubmit}>
+    <Form onSubmit={props.handleSubmit}>
       <Form.Group controlId='formInlineName'>
         <Form.Control
           value={props.comment}
@@ -12,7 +12,9 @@ const ReviewForm = (props) => {
           placeholder='write your commnets here..'
         />
       </Form.Group>
-      <Button type='submit'>Submit</Button>
+      <Button variant='primary' type='submit'>
+        Submit
+      </Button>
     </Form>
   );
 };
