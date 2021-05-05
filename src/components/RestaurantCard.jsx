64f16cf8fb7027f9854739bc4a17be3de31e4327
kilcmn3 <kilcmn3@hotmail.com>
2021-05-04 21:28:37 -0400

    display better each restaurant's contact

--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Card, Col, Row } from 'react-bootstrap';
+import { array } from 'yup';
 
 const RestaurantCard = (props) => {
-  let { name, phone, location, photos } = props.restaurant;
-  let address =
+  const { name, phone, location, photos } = props.restaurant;
+  const _address =
     JSON.parse(location).address1 +
     ', ' +
     JSON.parse(location).city +
@@ -13,6 +14,15 @@ const RestaurantCard = (props) => {
     ' ' +
     JSON.parse(location).zip_code;
 
+  const displayPhone = () => {
+    return (
+      phone.substring(2, 5) +
+      ') ' +
+      phone.substring(5, 8) +
+      ' - ' +
+      phone.substring(8)
+    );
+  };
   return (
     <Row className='justify-content-md-center'>
       <Col lg='12'>
@@ -24,10 +34,12 @@ const RestaurantCard = (props) => {
               <h4> {name}</h4>
               <p>
                 <small>
-                  <em>{address}</em>
+                  <em>{_address}</em>
                 </small>
               </p>
-              <span className='badge badge-info badge-pill'>{phone}</span>
+              <span className='badge badge-info badge-pill'>
+                {displayPhone()}
+              </span>
             </div>
             <div className='image-parent'>
               <Card.Img src={photos[0]} style={{ height: '200px' }} />
